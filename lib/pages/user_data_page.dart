52d1b77f62febba9c7108e8e37be6bfbbb4f7564
Themalka Anjala <themalka12345@gmail.com>
2025-02-20 17:32:27 +0530

    Form Validation Added

--- lib/pages/user_data_page.dart
+++ lib/pages/user_data_page.dart
@@ -1,4 +1,8 @@
+import 'package:expenz_app/constants/colors.dart';
+import 'package:expenz_app/constants/constants.dart';
+import 'package:expenz_app/widgets/custom_button.dart';
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
 class UserDataScreen extends StatefulWidget {
   const UserDataScreen({super.key});
@@ -8,8 +12,202 @@ class UserDataScreen extends StatefulWidget {
 }
 
 class _UserDataScreenState extends State<UserDataScreen> {
+  // Toggle switch state
+  bool _rememberMe = false;
+
+  // Form Key for the Validations
+  final _formKey = GlobalKey<FormState>();
+
+  // Input Field Controllers
+  final TextEditingController _userNameController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _confirmPasswordController =
+      TextEditingController();
+
+  @override
+  void dispose() {
+    _userNameController.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
+    _confirmPasswordController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(kDefaultPadding),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Lottie.asset(
+                  "assets/animation/Register.json",
+                  height: 240,
+                  width: 240,
+                  fit: BoxFit.cover,
+                ),
+                Text(
+                  "Enter Your Personal Details",
+                  style: TextStyle(
+                    color: kBlack,
+                    fontSize: 26,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                const SizedBox(height: 20),
+
+                // Form
+                Form(
+                  key: _formKey,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      // Form Field for Name
+                      TextFormField(
+                        controller: _userNameController,
+                        validator: (value) {
+                          // Check Whether User Entered Username
+                          if (value!.isEmpty) {
+                            return "Please Enter Your Name";
+                          }
+                        },
+                        decoration: InputDecoration(
+                          hintText: "Name",
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                          contentPadding: EdgeInsets.symmetric(
+                            vertical: 15,
+                            horizontal: 20,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 10),
+
+                      // Form Field for Email
+                      TextFormField(
+                        controller: _emailController,
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return "Please Enter Your Email";
+                          }
+                        },
+                        decoration: InputDecoration(
+                          hintText: "Email",
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                          contentPadding: EdgeInsets.symmetric(
+                            vertical: 15,
+                            horizontal: 20,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 10),
+
+                      // Form Field for Password
+                      TextFormField(
+                        controller: _passwordController,
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return "Please Enter a Valid Password";
+                          }
+                        },
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          hintText: "Password",
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                          contentPadding: EdgeInsets.symmetric(
+                            vertical: 15,
+                            horizontal: 20,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 10),
+
+                      // Form Field for Confirm Password
+                      TextFormField(
+                        controller: _confirmPasswordController,
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return "Please Enter the Same Password";
+                          }
+                        },
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          hintText: "Confirm Password",
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                          contentPadding: EdgeInsets.symmetric(
+                            vertical: 15,
+                            horizontal: 20,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+
+                      // Remember Me Toggle
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "Remember Me for the next time",
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w500,
+                              color: kGrey,
+                            ),
+                          ),
+                          Switch(
+                            value: _rememberMe,
+                            onChanged: (value) {
+                              setState(() {
+                                _rememberMe = value;
+                              });
+                            },
+                            activeColor: Colors.white,
+                            activeTrackColor: kMainColor,
+                          ),
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+
+                      // Submit Button
+                      GestureDetector(
+                          onTap: () {
+                            if (_formKey.currentState!.validate()) {
+                              // Form is Valid; Proces Data
+                              String userName = _userNameController.text;
+                              String email = _emailController.text;
+                              String password = _passwordController.text;
+                              String confirmPassword =
+                                  _confirmPasswordController.text;
+                            }
+                          },
+                          child: CustomButton(
+                              buttonName: "Next", buttonColor: kMainColor)),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
